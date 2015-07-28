4a6924635b7fd96279f0e93b9f70ff814ed57236
Aleksandr Cherednikov <aleksandr.cherednikov.tgn@gmail.com>
2015-07-28 20:15:05 +0300

    update iframe content js

--- public/mo_highlight.js
+++ public/mo_highlight.js
@@ -1,16 +1,31 @@
-$( document ).ready(function() {
-  console.log( "ready!" );
+function getPathTo(element) {
+  if (element.id!=='')
+    return 'id("'+element.id+'")';
+  if (element===document.body)
+    return element.tagName;
 
-  document.body.onmouseover = function(ev) {
-    return $(ev.target).css({
-      boxShadow: '0px 0px 5px red'
-    });
-  };
+  var ix= 0;
+  var siblings= element.parentNode.childNodes;
+  for (var i= 0; i<siblings.length; i++) {
+    var sibling= siblings[i];
+    if (sibling===element)
+      return getPathTo(element.parentNode)+'/'+element.tagName+'['+(ix+1)+']';
+    if (sibling.nodeType===1 && sibling.tagName===element.tagName)
+      ix++;
+  }
+}
 
-  document.body.onmouseout = function(ev) {
-    return $(ev.target).css({
-      boxShadow: ''
-    });
-  };
+document.body.onmouseover= function(ev){
+  ev.target.style.boxShadow = "0px 0px 5px red";
+  curXpath = getPathTo(ev.target);
+};
 
-});
+document.body.onmouseout= function(ev){
+  ev.target.style.boxShadow = "0px 0px 0px";
+};
+
+document.body.onclick = function(ev){
+  window.parent.postMessage(curXpath, "*");
+  ev.preventDefault();
+  ev.stopPropagation();
+};
